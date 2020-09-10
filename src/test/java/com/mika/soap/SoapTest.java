7b55c6fd068ce835a2fd5e56cee83a8308f7eadd
Mika Hamalainen <mika.hamalainen@gmail.com>
2020-09-10 20:54:01 +0300

    Initial commit

---
+++ src/test/java/com/mika/soap/SoapTest.java
@@ -0,0 +1,10 @@
+package com.mika.soap;
+
+import org.junit.jupiter.api.Test;
+
+public class SoapTest {
+    @Test
+    public void helloSoap() {
+        System.out.println("Hello, SOAP!");
+    }
+}
