4fdaa46eca689f2929c68befbd562eed60f41174
Mika Hamalainen <mika.hamalainen@gmail.com>
2020-09-10 21:23:52 +0300

    SOAPMessage to text

--- src/test/java/com/mika/soap/SoapTest.java
+++ src/test/java/com/mika/soap/SoapTest.java
@@ -2,9 +2,18 @@ package com.mika.soap;
 
 import org.junit.jupiter.api.Test;
 
+import javax.xml.soap.MessageFactory;
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+
 public class SoapTest {
     @Test
-    public void helloSoap() {
-        System.out.println("Hello, SOAP!");
+    public void helloSoap() throws Exception {
+        var factory = MessageFactory.newInstance();
+        var message = factory.createMessage();
+        var out = new ByteArrayOutputStream();
+        message.writeTo(out);
+        var xml = new String(out.toByteArray(), StandardCharsets.UTF_8);
+        System.out.println(xml);
     }
 }
